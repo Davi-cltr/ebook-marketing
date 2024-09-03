fb38655f4adf59ad4394d5a804399d7bfdb81a0c
Davi-cltr <mottapelucidavi@gmail.com>
2024-09-03 11:39:24 -0300

    Add files via upload

---
+++ minha_pag/script.js
@@ -0,0 +1,5 @@
+document.getElementById("buy-btn").addEventListener("click", function() {
+    alert("Obrigado por adquirir o eBook! Você será redirecionado para a página de pagamento.");
+    // Aqui você pode integrar com a página de pagamento ou plataforma de e-commerce
+    window.location.href = "https://kiwify.com.br";
+});
